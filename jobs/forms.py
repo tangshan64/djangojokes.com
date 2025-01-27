8e3d718c3de861caf79c976955b65df2b8f7e43f
Jeff Zuckerberg <tangshan64@gmail.com>
2025-01-27 16:57:52 -0500

    Jobs App (jobs.py) third commit

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django import forms
 
 class JobApplicationForm(forms.Form):
@@ -15,21 +16,43 @@ class JobApplicationForm(forms.Form):
         (4, 'THU'),
         (5, 'FRI')
     )
+    YEARS = range(datetime.now().year, datetime.now().year+2)
 
-    first_name = forms.CharField()
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(required=False)
+    website = forms.URLField(
+        required=False,
+        widget=forms.URLInput(
+            attrs={'placeholder':'https://www.example.com', 'size':'50'}
+        )
+    )
     employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
     start_date = forms.DateField(
-        help_text='The earliest date you can start working.'
+        help_text='The earliest date you can start working.',
+        widget=forms.SelectDateWidget(
+            years=YEARS,
+        )
     )
+
     available_days = forms.MultipleChoiceField(
         choices=DAYS,
-        help_text='Select all days that you can work.'
+        help_text='Select all days that you can work.',
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked':True}
+        )
     )
-    desired_hourly_wage = forms.DecimalField()
-    cover_letter = forms.CharField()
+    desired_hourly_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={'min':'10.00', 'max':'100.00', 'step':'.25'}
+        )
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'})
+    )
     confirmation = forms.BooleanField(
         label='I certify that the information I have provided is true.'
     )
