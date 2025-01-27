065904718f1791c6e42a47df44f25004b933d719
Jeff Zuckerberg <tangshan64@gmail.com>
2025-01-26 22:13:48 -0500

    Amazon SES commit

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -138,3 +138,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Amazon SES Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'email-smtp.us-east-2.amazonaws.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' 
+DEFAULT_FROM_EMAIL = 'tangshan64@gmail.com'
