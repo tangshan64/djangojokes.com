391837b9a0457c17f5b9b2a01dff13698d93336a
Jeff Zuckerberg <tangshan64@gmail.com>
2025-01-27 17:38:31 -0500

    Crispy Forms commit

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -40,12 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # Local apps
     'common.apps.CommonConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
