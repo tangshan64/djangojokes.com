460f331931832f81d0cd2748c5a7cb03bcdd958b
Jeff Zuckerberg <tangshan64@gmail.com>
2025-02-07 16:57:31 -0500

    Tags commit

--- jokes/models.py
+++ jokes/models.py
@@ -9,6 +9,7 @@ class Joke(models.Model):
     category = models.ForeignKey(
        'Category', on_delete=models.PROTECT
     )
+    tags = models.ManyToManyField('Tag', blank=True)
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
@@ -49,4 +50,25 @@ class Category(models.Model):
         return self.category
 
     class Meta:
-        verbose_name_plural = 'Categories'        
+        verbose_name_plural = 'Categories'
+        ordering = ['category']
+
+class Tag(models.Model):
+   tag = models.CharField(max_length=50)
+   slug = models.SlugField(
+       max_length=50, unique=True, null=False, editable=False
+   )
+   created = models.DateTimeField(auto_now_add=True)
+   updated = models.DateTimeField(auto_now=True)
+   def get_absolute_url(self):
+       return reverse('jokes:tag', args=[self.slug])
+   def save(self, *args, **kwargs):
+       if not self.slug:
+           value = str(self)
+           self.slug = unique_slug(value, type(self))
+       super().save(*args, **kwargs)
+   def __str__(self):
+       return self.tag  
+
+   class Meta:
+        ordering = ['tag']     
