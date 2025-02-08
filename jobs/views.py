137cd4a37ffdafc67ab85373e905574938b3ca4e
Jeff Zuckerberg <tangshan64@gmail.com>
2025-02-07 19:30:09 -0500

    Tying Job Applications to Models II commit

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,12 @@
+import html
 from django.urls import reverse_lazy
-from django.views.generic import FormView, TemplateView
+from django.views.generic import CreateView, TemplateView
 
+from .models import Applicant
 from .forms import JobApplicationForm
 
-class JobAppView(FormView):
-    template_name = 'jobs/joke_writer.html'
+class JobAppView(CreateView):
+    model = Applicant
     form_class = JobApplicationForm
     success_url = reverse_lazy('jobs:thanks')
 
